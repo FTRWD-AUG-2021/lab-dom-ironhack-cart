2ed79f1f32c390c5392305ce64b334765bd2ccab
Julian Phillips <julian@julianlp.com>
2021-08-25 10:27:55 -0600

    Class review

--- js/index.js
+++ js/index.js
@@ -4,20 +4,41 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotalCell = product.querySelector('.subtotal span');
+
+  const subtotal = price * quantity;
+  subtotalCell.innerHTML = subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const products = document.querySelectorAll('.product'); // returns a NodeList which does have forEach
+  // const products = document.getElementsByClassName('product'); //returns a HTMLCollection which does not have the forEach method
+  console.log(products);
+
+  let total = 0;
+  products.forEach((product) => {
+    updateSubtotal(product);
+    total += Number(product.querySelector('.subtotal span').innerText);
+  });
+
+  // for (let i = 0; i < products.length; i++) {
+  //   updateSubtotal(products[i]);
+  // }
 
   // ITERATION 3
   //... your code goes here
+  const totalPriceText = document.querySelector('#total-value span');
+  totalPriceText.innerHTML = total;
 }
 
 // ITERATION 4
